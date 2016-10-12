60726d19eb917287ab8fda07a312fc05f566aa9c
Hasib Billah <hasib.7724@gmail.com>
2016-10-12 23:39:55 +0600

    Minor imports reordering

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
 import TabPane from './TabPane';
-import { showcaseItems } from '../static-content.js';
 import appStore from '../stores/AppStore';
 
+import { showcaseItems } from '../static-content.js';
 import '../styles/App.css';
 
 const App = observer(class App extends Component {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
-import { libraries } from '../static-content.js';
 import appStore from '../stores/AppStore';
 
+import { libraries } from '../static-content.js';
 import '../styles/Search.css';
 
 const Search = observer(class Search extends Component {
