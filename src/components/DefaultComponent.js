c2d6b4b200c51cd04214b6380e5bb2acf29e1964
Hasib Billah <hasib.7724@gmail.com>
2016-10-13 16:39:16 +0600

    Default component doesn't require to be an obsrever

--- src/components/DefaultComponent.js
+++ src/components/DefaultComponent.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
 
-const DefaultComponent = observer(class DefaultComponent extends Component {
+class DefaultComponent extends Component {
   render() {
     return (
       <div className="DefaultComponent">Hello World!</div>
     )
   }
-})
+}
 
 export default DefaultComponent;
